ffc3c4558b8ad76035c58b68f3dbc62709b79a19
Martin Šmid <martin.smid@gmail.com>
2017-01-12 17:54:02 +0100

    Split ui and runner Support different runners Many other changes

--- putr.py
+++ putr.py
@@ -8,7 +8,7 @@ import pytest
 import logging
 import logging_tools
 import thread
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 import unittest
 from StringIO import StringIO
 import __builtin__
@@ -41,18 +41,6 @@ class RollbackImporter:
         __builtin__.__import__ = self.realImport
 
 
-def result_state(test_result):
-    if not test_result:
-        return ''
-
-    if test_result.skipped:
-        return 'skipped'
-    elif test_result.failures:
-        return 'failed'
-    elif test_result.errors:
-        return 'error'
-
-    return 'ok'
 
 def get_tests(suite):
     test_list = {}
@@ -67,12 +55,6 @@ def get_tests(suite):
 def exit_program(button):
     raise urwid.ExitMainLoop()
 
-
-def get_test_id(test):
-    if hasattr(test, 'id'):
-        return test.id()
-    elif hasattr(test, 'nodeid'):
-        return test.nodeid.replace('/', '.')
 
 class TestLine(urwid.Widget):
     _sizing = frozenset(['flow'])
@@ -89,14 +71,13 @@ class TestLine(urwid.Widget):
         return 1
 
     def render(self, size, focus=False):
-        result_state_str = result_state(self.test_data.get('result'))
-        test_id = get_test_id(self.test_data['suite'])
+        result_state_str = self.test_data.get('result_state')
         (maxcol,) = size
         attr = []
         main_attr = ('running', maxcol - 13) if self._is_running else (None, maxcol - 13)
         state_attr = (result_state_str, 10)
 
-        return urwid.TextCanvas(['{} [{:10}]'.format(test_id.ljust(maxcol - 13), result_state_str[:10])],
+        return urwid.TextCanvas(['{} [{:10}]'.format(self.test_data['id'].ljust(maxcol - 13), result_state_str[:10])],
             maxcol=maxcol, attr=[[main_attr, (None, 2), state_attr, (None, 1)]])
 
     def keypress(self, size, key):
@@ -109,9 +90,9 @@ class TestLine(urwid.Widget):
 class StatusLine(urwid.Widget):
     _sizing = frozenset(['flow'])
 
-    def __init__(self, status, *args, **kwargs):
+    def __init__(self, runner, *args, **kwargs):
         super(StatusLine, self).__init__(*args, **kwargs)
-        self.status = status
+        self.runner = runner
 
     def rows(self, size, focus=False):
         return 1
@@ -119,10 +100,11 @@ class StatusLine(urwid.Widget):
     def render(self, size, focus=False):
         (maxcol,) = size
 
+        stats = self.runner.get_test_stats()
         return urwid.TextCanvas(
-            ['Total: {} Filtered: {} Failed: {}'.format(self.status.total,
-                                                        self.status.filtered,
-                                                        self.status.failed)],
+            ['Total: {} Filtered: {} Failed: {}'.format(stats.total,
+                                                        stats.filtered,
+                                                        stats.failed)],
             maxcol=maxcol)
 
 
@@ -158,47 +140,164 @@ class TestResultWindow(urwid.LineBox):
         self._original_widget.set_focus(item)
 
 class PutrPytestPlugin(object):
-    def __init__(self, ui, test_list=None):
-        self.ui = ui
-        self.test_list = test_list
+    def __init__(self, runner, test_list={}):
+        self.runner = runner
 
     def pytest_runtest_protocol(self, item, nextitem):
         logger.debug('pytest_runtest_protocol %s %s', item, nextitem)
 
-    def pytest_runtest_makereport(self, item, call):
-        logger.debug('pytest_runtest_makereport %s %s', item, call)
-
     def pytest_itemcollected(self, item):
         logger.debug('pytest_itemcollected %s', item)
-        self.ui.add_test(item)
+        self.runner.add_test(item)
 
     def pytest_collectstart(self, collector):
         logger.debug('pytest_collectstart %s', collector)
 
     def pytest_runtest_makereport(self, item, call):
+        if call.when == 'call':
+            self.runner.set_test_result(self.runner.get_test_id(item), call)
+
         logger.debug('pytest_runtest_makereport %s %s', item, call)
 
-    def pytest_collection_modifyitems(self, session, config, items):
-        logger.debug('pytest_collection_modifyitems %s %s %s', session, config, items)
-        def wanted(test):
-            return get_test_id(test.nodeid) in wanted.test_keys
-        wanted.test_keys = self.test_list.keys()
+    def pytest_collectreport(self, report):
+        logger.debug('pytest_collectreport %s', report)
 
-        items[:] = filter(wanted, items)
+    def pytest_collection_modifyitems(self, session, config, items):
+        # import pudb; pu.db
+        logger.debug('pytest_collection_modifyitems %s %s %s', session, config, items)
+
+        items[:] = filter(self.runner.is_test_filtered, items)
         logger.debug('Filtered items %s', items)
 
-    def pytest_ignore_collect(self, path, config):
-        """ Return True to ignore the test """
-        # logger.debug('pytest_ignore_collect %s %s', path, config)
-        # logger.debug('path %s', type(path))
-        return False
-        if self.test_list and path not in self.test_list.keys():
-            logger.debug('Ignoring')
+
+class Runner(object):
+    def __init__(self, path='.', load_tests=True):
+        self.ui = None
+        self.path = path
+        self.tests = {}
+        # self.rollbackImporter = RollbackImporter()
+
+        if load_tests:
+            self.init_tests()
+            self.init_test_data()
+
+    def set_test_result(self, test_id, result, output='TODO'):
+        self.test_data[test_id].update({
+            'result': result,
+            'output': output,
+            'result_state': self.result_state(result)
+        })
+
+
+        self.ui.update_test_result(test_id)
+
+    def get_test_id(self, test):
+        raise NotImplementedError()
+
+    def is_test_filtered(self, test):
+        if not self.ui:
             return True
 
+        return self.get_test_id(test) in self.ui.current_test_list.keys()
+
+    def get_test_stats(self):
+        res = {
+            'total': 0,
+            'filtered': 0,
+            'failed': 0
+        }
+        for test_id, test in self.tests.iteritems():
+            res['total'] += 1
+            if self.test_data[test_id].get('result_state') in self._test_fail_states:
+                res['failed'] += 1
+
+            if self.is_test_filtered(test):
+                res['filtered'] += 1
+
+    def _get_tests(self, failed_only=True, filtered=True):
+        tests = self.ui.current_test_list if filtered else self.tests
+
+        return OrderedDict([(test_id, test) for test_id, test in tests.iteritems()
+                                  if not failed_only
+                                      or failed_only
+                                      and self.test_data[test_id].get('result_state') in self._test_fail_states])
 
 
-class TestRunner(object):
+class UnittestRunner(Runner):
+    def get_test_id(self, test):
+        return test.id()
+
+    def init_tests(self):
+        loader = unittest.TestLoader()
+        top_suite = loader.discover(self.path)
+        self.tests = get_tests(top_suite)
+
+    def init_test_data(self):
+        self.test_data = {test_id: {'suite': test} for test_id, test in self.tests.iteritems()}
+        self.current_test_list = self.tests
+        logger.debug('Inited tests %s', self.test_data)
+
+    def reload_tests(self):
+        if self.rollbackImporter:
+            self.rollbackImporter.uninstall()
+        self.rollbackImporter = RollbackImporter()
+        self.init_tests()
+
+    def run_tests(self, failed_only=True, filtered=True):
+        self.reload_tests()
+        self._first_failed_focused = False
+        tests = self._get_tests(failed_only, filtered)
+
+        for test_id, suite in tests.iteritems():
+            self._run_test(test_id)
+
+    def result_state(self, test_result):
+        if not test_result:
+            return ''
+        elif test_result.skipped:
+            return 'skipped'
+        elif test_result.failures:
+            return 'failed'
+        elif test_result.errors:
+            return 'error'
+
+        return 'ok'
+
+
+class PytestRunner(Runner):
+    _test_fail_states = ['failed', 'error', None]
+
+    def get_test_id(self, test):
+        return test.nodeid #.replace('/', '.')
+
+    def init_tests(self):
+        pytest.main(['-q', '-s', '--collect-only', self.path], plugins=[PutrPytestPlugin(self)])
+
+    def init_test_data(self):
+        self.test_data = {test_id: {'suite': test} for test_id, test in self.tests.iteritems()}
+        logger.debug('Inited tests %s', self.test_data)
+
+    def add_test(self, item):
+        self.tests[self.get_test_id(item)] = item
+
+    def run_tests(self, failed_only=True, filtered=True):
+        self._running_tests = True
+        pytest.main(['-q', '-s', self.path],
+            plugins=[PutrPytestPlugin(self, self._get_tests(failed_only, filtered))])
+        self._running_tests = False
+
+    def result_state(self, callinfo):
+        if not callinfo:
+            return ''
+        elif not callinfo.excinfo:
+            return 'passed'
+        else:
+            return 'failed or skipped'
+
+        return 'ok'
+
+
+class TestRunnerUI(object):
     palette = [
         ('reversed',    '',           'dark gray'),
         ('edit',        '',           'dark blue',    '', '',     '#008'),
@@ -211,77 +310,31 @@ class TestRunner(object):
         ('statusline',  'white',      'dark blue',    '', '',     ''),
     ]
 
-    _test_fail_states = ['failed', 'error', None]
-
-    def __init__(self, path='.', load_tests=True, runner='pytest'):
-        logger.info('Runner init')
+    def __init__(self, runner):
+        logger.info('Runner UI init')
         urwid.set_encoding("UTF-8")
 
-        # self.rollbackImporter = RollbackImporter()
-        self.current_test_list = {}
-        self.tests = {}
+        self.main_loop = None
+        self.test_data = defaultdict(dict)
+        self.re_filter = None
         self.runner = runner
-        self.path = path
-
-        if load_tests:
-            self.init_tests()
-            self.init_test_data()
+        self.runner.ui = self
 
         self.init_main_screen()
-        self.main_loop = None
         self._running_tests = False
-
-    @property
-    def filtered(self):
-        return len(self.current_test_list)
-
-    @property
-    def total(self):
-        return len(self.tests)
-
-    @property
-    def failed(self):
-        return len(self._get_tests(failed_only=True))
-
-    def init_tests(self):
-        if self.runner == 'unittest':
-            self.init_tests_unittest()
-        elif self.runner == 'pytest':
-            self.init_tests_pytest()
-
-    def init_tests_unittest(self):
-        loader = unittest.TestLoader()
-        top_suite = loader.discover(self.path)
-        self.tests = get_tests(top_suite)
-
-    def init_tests_pytest(self):
-        pytest.main(['-q', '-s', '--collect-only', self.path], plugins=[PutrPytestPlugin(self)])
-
-    def init_test_data(self):
-        self.test_data = {test_id: {'suite': test} for test_id, test in self.tests.iteritems()}
-        self.current_test_list = self.tests
-        logger.debug('Inited tests %s', self.test_data)
-        self._init_test_listbox()
+        self.init_test_listbox()
 
     def add_test(self, item):
-        # import pdb; pdb.set_trace()
         if item.nodeid in self.tests:
             logging.warn('Duplicate test id collected %s', item.nodeid)
 
         self.tests[item.nodeid] = item
 
-    def reload_tests(self):
-        if self.runner == 'unittest':
-            if self.rollbackImporter:
-                self.rollbackImporter.uninstall()
-            self.rollbackImporter = RollbackImporter()
-        self.init_tests()
-
     def init_main_screen(self):
         self.w_filter_edit = urwid.AttrMap(urwid.Edit('Filter '), 'edit', 'edit_focus')
-        self.w_status_line = urwid.AttrMap(StatusLine(self), 'statusline', '')
+        self.w_status_line = urwid.AttrMap(StatusLine(self.runner), 'statusline', '')
         urwid.connect_signal(self.w_filter_edit.original_widget, 'change', self.on_filter_change)
-        self._init_test_listbox()
+        self.init_test_listbox()
         self.w_main = urwid.Padding(
             urwid.Pile([
                 ('pack', urwid.Text(u'Python Urwid Test Runner', align='center')),
@@ -295,16 +348,29 @@ class TestRunner(object):
             left=2, right=2
         )
 
-    def _init_test_listbox(self):
+    def on_testlist_change(self):
+        self.init_test_listbox()
+
+    def init_test_listbox(self):
         self.w_test_listbox = self.test_listbox(self.current_test_list.keys())
 
+    @property
+    def current_test_list(self):
+        if not self.re_filter:
+            return self.runner.tests
+
+        current_test_list = {k: v for k, v in self.runner.tests.iteritems() if self.re_filter.findall(k)}
+        return current_test_list
+
     def on_filter_change(self, filter_widget, filter_value):
-        regexp_str = '.*?'.join(list(iter(
-            filter_value.replace('.', '\.').replace(r'\\', '\\\\')
-        )))
-        re_filter = re.compile(regexp_str, re.UNICODE + re.IGNORECASE)
-        self.current_test_list = {k: v for k, v in self.tests.iteritems() if re_filter.findall(k)}
-        matched_key = self.current_test_list.keys()[0]
+        if not filter_value:
+            self.re_filter = None
+        else:
+            regexp_str = '.*?'.join(list(iter(
+                filter_value.replace('.', '\.').replace(r'\\', '\\\\')
+            )))
+            self.re_filter = re.compile(regexp_str, re.UNICODE + re.IGNORECASE)
+
         self.w_main.original_widget.widget_list[4] = self.test_listbox(self.current_test_list.keys())
         self.w_main.original_widget._invalidate()
         self.w_status_line.original_widget._invalidate()
@@ -314,8 +380,6 @@ class TestRunner(object):
     def run(self):
         self.main_loop = urwid.MainLoop(self.w_main, palette=self.palette,
                        unhandled_input=self.unhandled_keypress)
-
-        # self._run_tests()
         self.main_loop.run()
 
     def popup(self, widget):
@@ -329,8 +393,18 @@ class TestRunner(object):
     def _get_test_position(self, test_id):
         return self.test_data[test_id]['position']
 
-    def _run_test(self, test_id):
+    def run_tests(self, failed_only=True, filtered=True):
+        self._running_tests = True
 
+        self.runner.run_tests(failed_only, filtered)
+
+        self.w_test_listbox._invalidate()
+        self.w_main._invalidate()
+        self.main_loop.draw_screen()
+        self._running_tests = False
+
+
+    def _run_test(self, test_id):
         self.test_data[test_id]['widget']._is_running = True
         self.test_data[test_id]['widget']._invalidate()
 
@@ -345,16 +419,8 @@ class TestRunner(object):
 
         suite = self.tests[test_id]
         result = unittest.TextTestRunner(stream=sys.stdout, verbosity=2).run(suite)
-        self.test_data[test_id]['result'] = result
-        result_state_str = result_state(result)
-        if result_state_str in ['failed', 'error'] and not self._first_failed_focused:
-            self.w_test_listbox.set_focus(self._get_test_position(test_id))
-            self._first_failed_focused = True
+        self.runner.set_test_result(test_id, result, sys.stdout.getvalue())
 
-        self.test_data[test_id].update({
-            'output': sys.stdout.getvalue(),
-            'result_state': result_state_str,
-        })
         sys.stdout.close()
         sys.stderr.close()
 
@@ -368,40 +434,17 @@ class TestRunner(object):
         self.w_status_line.original_widget._invalidate()
         self.main_loop.draw_screen()
 
-    def _get_tests(self, failed_only=True, filtered=True):
-        tests = self.current_test_list if filtered else self.tests
+    def update_test_result(self, test_id):
+        test_result = self.runner.test_data[test_id]
+        result_state_str = self.runner.result_state(test_result)
+        if result_state_str in ['failed', 'error'] and not self._first_failed_focused:
+            self.w_test_listbox.set_focus(self._get_test_position(test_id))
+            self._first_failed_focused = True
 
-        return OrderedDict([(test_id, test) for test_id, test in tests.iteritems()
-                                  if not failed_only
-                                      or failed_only
-                                      and self.test_data[test_id].get('result_state') in self._test_fail_states])
-
-    def _run_tests(self, failed_only=True, filtered=True):
-        self._running_tests = True
-        if self.runner == 'unittest':
-            self.reload_tests()
-            self._first_failed_focused = False
-            tests = self._get_tests(failed_only, filtered)
-
-            for test_id, suite in tests.iteritems():
-                self._run_test(test_id)
-
-            self.w_test_listbox._invalidate()
-            self.w_main._invalidate()
-            self.main_loop.draw_screen()
-        elif self.runner == 'pytest':
-            self._run_tests_pytest(failed_only, filtered)
-
-        self._running_tests = False
-
-    def _run_tests_pytest(self, failed_only=True, filtered=True):
-        pytest.main(['-q', '-s', self.path],
-            plugins=[PutrPytestPlugin(self, self._get_tests(failed_only, filtered))])
-
-    def show_test_detail(self, widget, choice):
+    def show_test_detail(self, widget, test_id):
         # if test has already been run
-        if 'output' in self.test_data[choice]:
-            result_window = TestResultWindow(self.test_data[choice]['output'], self.popup_close)
+        if 'output' in self.runner.test_data[test_id]:
+            result_window = TestResultWindow(self.runner.test_data[test_id]['output'], self.popup_close)
             self.popup(result_window)
             result_window.set_focus(0)
 
@@ -412,9 +455,9 @@ class TestRunner(object):
         list_items = []
         for position, test_id in enumerate(test_list):
             self.test_data[test_id].update({
-                'id': test_id,
                 'widget': None,
                 'position': position,
+                'id': test_id
             })
             test_line = TestLine(self.test_data[test_id])
             self.test_data[test_id]['widget'] = test_line
@@ -430,12 +473,12 @@ class TestRunner(object):
         elif key == 'R':
             if not self._running_tests:
                 thread.start_new_thread(
-                    self._run_tests, (False, )
+                    self.run_tests, (False, )
                 )
         elif key == 'r':
             if not self._running_tests:
                 thread.start_new_thread(
-                    self._run_tests, (True, )
+                    self.run_tests, (True, )
                 )
 
     def set_test_list(self, test_list):
@@ -445,5 +488,7 @@ if __name__ == '__main__':
     path = sys.argv[1] if len(sys.argv) - 1 else '.'
     logging_tools.configure()
     logger.debug('Configured logging')
-    runner = TestRunner(path, load_tests=True)
-    runner.run()
+
+    runner = PytestRunner(path)
+    ui = TestRunnerUI(runner)
+    ui.run()
