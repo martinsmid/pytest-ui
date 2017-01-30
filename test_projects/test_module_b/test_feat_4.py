9749cdaaebacd7316b581c5d368a5082fb6d3789
Martin Šmid <martin.smid@gmail.com>
2017-01-30 16:09:24 +0100

    Add empty time tests (for ui testing)

--- test_projects/test_module_b/test_feat_4.py
+++ test_projects/test_module_b/test_feat_4.py
@@ -1,3 +1,4 @@
+import time
 import unittest
 
 
@@ -17,13 +18,37 @@ class TestG(unittest.TestCase):
 
 class TestH(unittest.TestCase):
     def test_feat_1_case_1(self):
-        pass
+        time.sleep(0.1)
 
     def test_feat_1_case_2(self):
-        pass
+        time.sleep(0.1)
 
     def test_feat_1_case_3(self):
-        pass
+        time.sleep(0.1)
 
     def test_feat_1_case_4(self):
-        pass
+        time.sleep(0.1)
+
+    def test_feat_1_case_5(self):
+        time.sleep(0.1)
+
+    def test_feat_1_case_6(self):
+        time.sleep(0.1)
+
+    def test_feat_1_case_7(self):
+        time.sleep(0.1)
+
+    def test_feat_1_case_8(self):
+        time.sleep(0.1)
+
+    def test_feat_1_case_9(self):
+        time.sleep(0.1)
+
+    def test_feat_1_case_10(self):
+        time.sleep(0.1)
+
+    def test_feat_1_case_11(self):
+        time.sleep(0.1)
+
+    def test_feat_1_case_12(self):
+        time.sleep(0.1)
