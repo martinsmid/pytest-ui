64fa891fb4b5e393de0ddc2dedc4df1787e413f2
Martin Šmid <martin.smid@gmail.com>
2017-02-16 00:16:28 +0100

    Add exception traceback to output window Add Alt + Up/Down keys to jump to failed sibling

--- putr.py
+++ putr.py
@@ -6,6 +6,7 @@ import sys
 import urwid
 import pytest
 import logging
+import traceback
 import logging_tools
 import thread
 from collections import OrderedDict, defaultdict
@@ -72,7 +73,6 @@ class TestLine(urwid.Widget):
 
     def render(self, size, focus=False):
         result_state_str = self.test_data.get('result_state', '')
-        logger.debug('result_state_str %s', result_state_str)
         (maxcol,) = size
         attr = []
         main_attr = ('running', maxcol - 13) if self._is_running else (None, maxcol - 13)
@@ -153,15 +153,16 @@ class PutrPytestPlugin(object):
     def pytest_collectstart(self, collector):
         logger.debug('pytest_collectstart %s', collector)
 
+
+    def pytest_runtest_makereport(self, item, call):
+        logger.debug('pytest_runtest_makereport %s %s', item, str(type(call.excinfo)))
+        if call.excinfo:
+            self.runner.set_exc_info(item.nodeid, call.excinfo)
+
     def pytest_runtest_logreport(self, report):
-        logger.debug('pytest_runtest_logreport')
-        logger.debug('report %s: %s', report.nodeid, report)
-        logger.debug('report.capstdout %s', report.capstdout)
-        logger.debug('report.capstderr %s', report.capstderr)
         if (report.when == 'setup' and report.outcome == 'skipped'
             or report.when == 'call'):
             self.runner.set_test_result(report.nodeid, report, report.capstdout + report.capstderr)
-
 
     def pytest_collectreport(self, report):
         logger.debug('pytest_collectreport %s', report)
@@ -184,7 +185,7 @@ class Runner(object):
             self.init_tests()
             self.init_test_data()
 
-    def set_test_result(self, test_id, result, output='TODO'):
+    def set_test_result(self, test_id, result, output):
         logger.debug('result %s', result)
         self.test_data[test_id].update({
             'result': result,
@@ -192,8 +193,11 @@ class Runner(object):
             'result_state': self.result_state(result)
         })
 
-
         self.ui.update_test_result(test_id)
+
+    def set_exc_info(self, test_id, excinfo):
+        self.test_data[test_id]['exc_info'] = excinfo
+        self.test_data[test_id]['output'] = excinfo.getrepr()
 
     def get_test_id(self, test):
         raise NotImplementedError()
@@ -302,6 +306,21 @@ class PytestRunner(Runner):
         logger.warn('Unknown report outcome %s', report.outcome)
         return 'N/A'
 
+    def get_failed_sibling(self, test_id, direction):
+        tests = self._get_tests(True, True)
+        keys = tests.keys()
+        next_pos = keys.index(test_id) + direction
+        if not (next_pos >= 0 and next_pos < len(keys)):
+            return None
+
+        return keys[next_pos]
+
+    def get_next_failed(self, test_id):
+        return self.get_failed_sibling(test_id, 1)
+
+    def get_previous_failed(self, test_id):
+        return self.get_failed_sibling(test_id, -1)
+
 
 class TestRunnerUI(object):
     palette = [
@@ -339,15 +358,16 @@ class TestRunnerUI(object):
         self.tests[item.nodeid] = item
 
     def init_main_screen(self):
-        self.w_filter_edit = urwid.AttrMap(urwid.Edit('Filter '), 'edit', 'edit_focus')
+        self.w_filter_edit = urwid.Edit('Filter ')
+        aw_filter_edit = urwid.AttrMap(self.w_filter_edit, 'edit', 'edit_focus')
         self.w_status_line = urwid.AttrMap(StatusLine(self.runner), 'statusline', '')
-        urwid.connect_signal(self.w_filter_edit.original_widget, 'change', self.on_filter_change)
+        urwid.connect_signal(self.w_filter_edit, 'change', self.on_filter_change)
         self.init_test_listbox()
         self.w_main = urwid.Padding(
             urwid.Pile([
                 ('pack', urwid.Text(u'Python Urwid Test Runner', align='center')),
                 ('pack', urwid.Divider()),
-                ('pack', self.w_filter_edit),
+                ('pack', aw_filter_edit),
                 ('pack', urwid.Divider()),
                 self.w_test_listbox,
                 ('pack', urwid.Divider()),
@@ -449,7 +469,6 @@ class TestRunnerUI(object):
         test_data = self.runner.test_data[test_id]
         result_state_str = test_data.get('result_state')
         self.test_data[test_id]['result_state'] = result_state_str
-        logger.debug('result_state_str %s', result_state_str)
         if result_state_str in ['failed', 'error'] and not self._first_failed_focused:
             self.w_test_listbox.set_focus(self._get_test_position(test_id))
             self._first_failed_focused = True
@@ -462,9 +481,17 @@ class TestRunnerUI(object):
         self.main_loop.draw_screen()
 
     def show_test_detail(self, widget, test_id):
+        test_data = self.runner.test_data[test_id]
         # if test has already been run
-        if 'output' in self.runner.test_data[test_id]:
-            result_window = TestResultWindow(self.runner.test_data[test_id]['output'], self.popup_close)
+        if 'output' in test_data:
+            output = test_data['output']
+        if 'exc_info' in test_data:
+            output += '\n' + '-'*20 + '\n'
+            output += '\n'.join(traceback.format_tb(test_data['exc_info'].tb))
+
+            result_window = TestResultWindow(
+                output,
+                self.popup_close)
             self.popup(result_window)
             result_window.set_focus(0)
 
@@ -494,10 +521,22 @@ class TestRunnerUI(object):
             list_items.append(test_line_attr)
         return urwid.ListBox(urwid.SimpleFocusListWalker(list_items))
 
+    def focus_failed_sibling(self, direction):
+        tests = self.runner._get_tests(False, True)
+        test_id = tests.keys()[self.w_test_listbox.focus_position]
+        next_id = self.runner.get_failed_sibling(test_id, direction)
+        if next_id is not None:
+            next_pos = self._get_test_position(next_id)
+            self.w_test_listbox.set_focus(next_pos, 'above' if direction == 1 else 'below')
+            self.w_test_listbox._invalidate()
+
     def unhandled_keypress(self, key):
         if key in ('q', 'Q'):
             exit_program(None)
         elif key == '/':
+            self.w_main.original_widget.set_focus(2)
+        elif key == 'ctrl f':
+            self.w_filter_edit.set_edit_text('')
             self.w_main.original_widget.set_focus(2)
         elif key == 'R':
             if not self._running_tests:
@@ -509,9 +548,11 @@ class TestRunnerUI(object):
                 thread.start_new_thread(
                     self.run_tests, (True, )
                 )
+        elif key == 'meta down':
+            self.focus_failed_sibling(1)
 
-    def set_test_list(self, test_list):
-        self._test_list = test_list
+        elif key == 'meta up':
+            self.focus_failed_sibling(-1)
 
 if __name__ == '__main__':
     path = sys.argv[1] if len(sys.argv) - 1 else '.'
