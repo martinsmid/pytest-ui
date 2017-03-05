4a7c09f196b71e1aefee683f338a6acac446699a
Martin Šmid <martin.smid@gmail.com>
2017-03-05 12:25:04 +0100

    Add setup.py for packaging, move modules to their folder

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='pytest-ui',
+    description='Text User Interface for running python tests',
+    version='0.1',
+    license='MIT',
+    platforms=['linux', 'osx', 'win32'],
+    packages=['pytui'],
+    url='https://github.com/martinsmid/pytest-ui',
+    author_email='martin.smid@gmail.com',
+    author='Martin Smid',
+    entry_points={
+        'pytest11': [
+            'pytui = pytui.runner',
+        ]
+    },
+    install_requires=['urwid,pytest'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Utilities',
+        'Programming Language :: Python', ],
+)
