ddd7d07dcd596923cdec7d7602d687d55d77d09e
Martin Šmid <martin.smid@gmail.com>
2017-05-15 23:48:39 +0200

    Add and rearrange tests

--- test_projects/test_module_a/test_feat_2.py
+++ test_projects/test_module_a/test_feat_2.py
@@ -7,15 +7,3 @@ class TestFeatures(unittest.TestCase):
 
     def test_feat_1_case_2(self):
         pass
-
-    @unittest.skip
-    def test_unittest_skip(self):
-        raise Exception('This shouldn\'t run')
-
-    @pytest.mark.skip
-    def test_pytest_skip(self):
-        raise Exception('This shouldn\'t run')
-
-    @pytest.mark.xfail
-    def test_xfail(self):
-        raise Exception('This shouldn\'t run IMHO')
---
+++ test_projects/test_module_a/test_import_error_in_test.py
@@ -0,0 +1,5 @@
+
+class TestImportError(unittest.TestCase):
+    def test_import_error_inside(self):
+        # import error
+        pass
