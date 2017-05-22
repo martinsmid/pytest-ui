488462658b35d5ace206d948df7e81a5fc96f981
Martin Šmid <martin.smid@gmail.com>
2017-05-22 17:56:35 +0200

    Add xfail test combinations

--- test_projects/test_module_a/test_feat_2.py
+++ test_projects/test_module_a/test_feat_2.py
@@ -16,6 +16,18 @@ class TestFeatures(unittest.TestCase):
     def test_pytest_skip(self):
         raise Exception('This shouldn\'t run')
 
-    @pytest.mark.xfail
-    def test_xfail(self):
-        raise Exception('This shouldn\'t run IMHO')
+    @pytest.mark.xfail(reason='There is a bug somewhere')
+    def test_xfail_correct_easy(self):
+        raise Exception('This shouldn\'t run')
+
+    @pytest.mark.xfail(reason='There is a bug somewhere (but it disappeared!)')
+    def test_xfail_wrong_easy(self):
+        pass
+
+    @pytest.mark.xfail(reason='There is a bug somewhere', strict=True)
+    def test_xfail_correct_strict(self):
+        raise Exception('This shouldn\'t run')
+
+    @pytest.mark.xfail(reason='There is a bug somewhere (but it disappeared!)', strict=True)
+    def test_xfail_wrong_strict(self):
+        pass
